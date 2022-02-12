1b76c006ffbda4469a1b60b72e3aa0fa6fea09f6
YourLifeForAiur <r320stwg@yandex.ru>
2022-02-12 19:36:53 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+template <class Zerg>
+Zerg Max(Zerg a, Zerg b)
+{
+	if (a > b) return a;
+	return b;
+}
+
+void main()
+{
+	int R, K, maxInt;
+	cout << "En Taro Kerrigan! for Sabaton! for Rammstein!" << ::endl;
+	cout << "Vvedite pervoe ";
+	cin >> R;
+	cout << "Vvedite vtoroe ";
+	cin >> K;
+	maxInt = Max(R, K);
+	cout << "Bolshee: " << maxInt << endl;
+	system("pause");
+}
